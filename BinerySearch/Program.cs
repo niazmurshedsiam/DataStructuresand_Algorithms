b0fb92ae03314489b6be1b05d049c26c7d4047b1
Niaz Murshed Siam <niazmurshed16@gmail.com>
2023-08-14 13:15:07 +0600

    Binery Search

---
+++ BinerySearch/Program.cs
@@ -0,0 +1,42 @@
+﻿class BinerySearch
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 5, 7, 9, 12, 15, 20, 24, 27, 33, 40, 50, 65, 74 };
+        int target = 33;
+        int index = Binery_Search(array, target);
+        if(index != -1)
+        {
+            Console.WriteLine($"{target} found at index {index}.");
+        }
+        else
+        {
+            Console.WriteLine($"{target} not found in the array.");
+        }
+    }
+
+        static int Binery_Search(int[] arr, int target)
+    {
+        int n = arr.Length;
+        int l = 0, r = n - 1, mid;
+        while(l<=r)
+        {
+            mid = (l+r)/2;
+            if(target == arr[mid])
+            {
+                return mid;
+
+            }
+            else if(target > arr[mid]) {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+
+}
--- LinearSearch/Program.cs
+++ LinearSearch/Program.cs
@@ -5,7 +5,7 @@
         int[] array = { 64, 34, 25, 12, 22, 11, 90 };
         int target = 22;
         int index = Linear_Search(array, target);
-        if (index != -1)
+        if(index != 1)
         {
             Console.WriteLine($"{target} found at index {index}.");
         }
@@ -15,7 +15,7 @@
         }
     }
 
-    static int Linear_Search(int[] arr, int target)
+        static int Linear_Search(int[] arr,int target)
     {
         int n = arr.Length;
         for (int i = 0; i < n; i++)
@@ -24,8 +24,40 @@
             {
                 return i;
             }
-        }
 
+        }
         return -1;
     }
 }
+
+//class LinearSearch
+//{
+//    static void Main(string[] args)
+//    {
+//        int[] array = { 64, 34, 25, 12, 22, 11, 90 };
+//        int target = 22;
+//        int index = Linear_Search(array, target);
+//        if (index != -1)
+//        {
+//            Console.WriteLine($"{target} found at index {index}.");
+//        }
+//        else
+//        {
+//            Console.WriteLine($"{target} not found in the array.");
+//        }
+//    }
+
+//    static int Linear_Search(int[] arr, int target)
+//    {
+//        int n = arr.Length;
+//        for (int i = 0; i < n; i++)
+//        {
+//            if (arr[i] == target)
+//            {
+//                return i;
+//            }
+//        }
+
+//        return -1;
+//    }
+//}
