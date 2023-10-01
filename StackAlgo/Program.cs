44a60dcef3f2473d017e15fc485c97d77b240801
Niaz Murshed Siam <niazmurshed16@gmail.com>
2023-10-01 13:54:11 +0600

    queue

--- StackAlgo/Program.cs
+++ StackAlgo/Program.cs
@@ -2,13 +2,60 @@
 
 class Stack
 {
-    private char[] items;
+    private int maxSize;
     private int top;
+    private int[] stackArray;
 
     public Stack(int size)
     {
-        items = new char[size];
+        maxSize = size;
+        stackArray = new int[maxSize];
         top = -1; // Initialize top index to -1 (empty stack)
+    }
+
+    public void Push(int value)
+    {
+        if (top < maxSize - 1)
+        {
+            top++;
+            stackArray[top] = value;
+            Console.WriteLine($"Pushed {value} onto the stack.");
+        }
+        else
+        {
+            Console.WriteLine("Stack is full. Cannot push.");
+        }
+    }
+
+    public int Pop()
+    {
+        if (top >= 0)
+        {
+            int poppedValue = stackArray[top];
+            top--;
+            Console.WriteLine($"Popped {poppedValue} from the stack.");
+            return poppedValue;
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty. Cannot pop.");
+            return -1; // Return a sentinel value to indicate failure
+        }
+    }
+
+    public int Peek()
+    {
+        if (top >= 0)
+        {
+            int peekedValue = stackArray[top];
+            Console.WriteLine($"Peeked at {peekedValue} on top of the stack.");
+            return peekedValue;
+        }
+        else
+        {
+            Console.WriteLine("Stack is empty. Cannot peek.");
+            return -1; // Return a sentinel value to indicate failure
+        }
     }
 
     public bool IsEmpty()
@@ -18,61 +65,28 @@ class Stack
 
     public bool IsFull()
     {
-        return top == items.Length - 1;
-    }
-
-    public void Push(char value)
-    {
-        if (!IsFull())
-        {
-            top++;
-            items[top] = value;
-        }
-        else
-        {
-            Console.WriteLine("Stack is full. Cannot push.");
-        }
-    }
-
-    public char Pop()
-    {
-        if (!IsEmpty())
-        {
-            char poppedValue = items[top];
-            top--;
-            return poppedValue;
-        }
-        else
-        {
-            Console.WriteLine("Stack is empty. Cannot pop.");
-            return '\0'; // Return a sentinel value to indicate failure
-        }
+        return top == maxSize - 1;
     }
 }
 
 class Program
 {
-    static void ReverseString(char[] str)
-    {
-        Stack stack = new Stack(str.Length);
-
-        foreach (char ch in str)
-        {
-            stack.Push(ch);
-        }
-
-        for (int i = 0; i < str.Length; i++)
-        {
-            str[i] = stack.Pop();
-        }
-    }
-
     static void Main(string[] args)
     {
-        char[] str = "Hello, World!".ToCharArray();
-        Console.WriteLine($"Original string: {new string(str)}");
+        Stack stack = new Stack(5);
 
-        ReverseString(str);
-        Console.WriteLine($"Reversed string: {new string(str)}");
+        stack.Push(10);
+        stack.Push(20);
+        stack.Push(30);
+        stack.Push(30);
+
+        int poppedValue = stack.Pop();
+        Console.WriteLine($"Popped value: {poppedValue}");
+
+        int peekedValue = stack.Peek();
+        Console.WriteLine($"Peeked value: {peekedValue}");
+
+        Console.WriteLine($"Is stack empty? {stack.IsEmpty()}");
+        Console.WriteLine($"Is stack full? {stack.IsFull()}");
     }
 }
