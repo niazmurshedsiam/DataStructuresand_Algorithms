5aa5afa558c92497ef248976845d06db605b820b
Niaz Murshed Siam <niazmurshed16@gmail.com>
2023-10-02 12:18:14 +0600

    Circular Queue

---
+++ CircularQueue/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+public class CircularQueue
+{
+    private int[] queue;
+    private int front;
+    private int rear;
+    private int capacity;
+
+    public CircularQueue(int size)
+    {
+        capacity = size;
+        queue = new int[capacity];
+        front = -1;
+        rear = -1;
+    }
+
+    public void Enqueue(int item)
+    {
+        // Check if the queue is full
+        if ((rear + 1) % capacity == front)
+        {
+            Console.WriteLine("Queue is full. Cannot enqueue.");
+            return;
+        }
+
+        // If the queue is empty, set front to 0
+        if (front == -1)
+            front = 0;
+
+        // Increment rear and wrap around if needed
+        rear = (rear + 1) % capacity;
+        queue[rear] = item;
+        Console.WriteLine("Enqueued: " + item);
+    }
+
+    public void Dequeue()
+    {
+        // Check if the queue is empty
+        if (front == -1)
+        {
+            Console.WriteLine("Queue is empty. Cannot dequeue.");
+            return;
+        }
+
+        int item = queue[front];
+
+        // If there's only one element, reset front and rear
+        if (front == rear)
+        {
+            front = -1;
+            rear = -1;
+        }
+        else
+        {
+            // Move front forward and wrap around if needed
+            front = (front + 1) % capacity;
+        }
+
+        Console.WriteLine("Dequeued: " + item);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CircularQueue cq = new CircularQueue(5);
+        cq.Enqueue(10);
+        cq.Enqueue(20);
+        cq.Enqueue(30);
+        cq.Enqueue(40);
+        cq.Enqueue(50);
+
+        // Queue is full, cannot enqueue
+        cq.Enqueue(60);
+
+        cq.Dequeue();
+        cq.Dequeue();
+        cq.Dequeue();
+        cq.Dequeue();
+        cq.Dequeue();
+
+        // Queue is empty, cannot dequeue
+        cq.Dequeue();
+    }
+}
+
--- Queue/Program.cs
+++ Queue/Program.cs
@@ -1,2 +1,88 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class QueueExample
+{
+    private int[] queueArray;
+    private int front; // Index of the front element
+    private int rear;  // Index of the rear element
+    private int capacity;
+
+    public QueueExample(int size)
+    {
+        capacity = size;
+        queueArray = new int[size];
+        front = -1;
+        rear = -1;
+    }
+
+    public void Enqueue(int item)
+    {
+        if (IsFull())
+        {
+            Console.WriteLine("Queue is full. Cannot enqueue.");
+            return;
+        }
+
+        if (front == -1)
+            front = 0;
+
+        rear++;
+        queueArray[rear] = item;
+        Console.WriteLine($"Enqueued: {item}");
+    }
+
+    public int Dequeue()
+    {
+        if (IsEmpty())
+        {
+            Console.WriteLine("Queue is empty. Cannot dequeue.");
+            return -1; // Return a sentinel value to indicate failure
+        }
+
+        int dequeuedItem = queueArray[front];
+        front++;
+
+        if (front > rear)
+        {
+            // Reset front and rear when the queue becomes empty
+            front = -1;
+            rear = -1;
+        }
+
+        Console.WriteLine($"Dequeued: {dequeuedItem}");
+        return dequeuedItem;
+    }
+
+    public bool IsEmpty()
+    {
+        return front == -1;
+    }
+
+    public bool IsFull()
+    {
+        return rear == capacity - 1;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        QueueExample queue = new QueueExample(5);
+
+        queue.Enqueue(10);
+        queue.Enqueue(20);
+        queue.Enqueue(30);
+
+        queue.Dequeue();
+        queue.Dequeue();
+        queue.Dequeue();
+        queue.Dequeue(); // Attempting to dequeue when the queue is empty
+
+        queue.Enqueue(40);
+        queue.Enqueue(50);
+
+        Console.ReadKey();
+    }
+}
+
